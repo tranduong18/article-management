230716fd8279664f55f66cd52f9a9904b479c48e
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-27 15:20:11 +0700

    2.2. Kết nối Database

---
+++ config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Kết nối database thành công!");
+    } catch (error) {
+        // console.log(error);
+        console.log("Kết nối database thất bại");
+    }
+}
--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
 import express,{ Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDatabase } from "./config/database";
+
+import Article from "./models/article.model";
+
+connectDatabase();
 
 const app: Express = express();
 const port: number = 3000;
 
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+app.get("/articles", async (req: Request, res: Response) => {
+    const articles = await Article.find({
+        deleted: false
+    });
+
     res.json({
-        articles: []
+        articles: articles
     });
 });
 
