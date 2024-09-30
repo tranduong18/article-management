c8c642aafc3cc2c8c7c31180df039412480e45a5
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-30 09:56:55 +0700

    3. Quản lý Danh mục bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,4 +1,5 @@
 import Article from "./models/article.model";
+import Category from "./models/category.model";
 
 export const resolvers = {
   Query: {
@@ -19,6 +20,25 @@ export const resolvers = {
       })
 
       return article;
+    },
+
+    getListCategory: async () => {
+      const categories = await Category.find({
+        deleted: false
+      });
+
+      return categories;
+    },
+
+    getCategory: async (_, args) => {
+      const { id } = args;
+
+      const category = await Category.findOne({
+        _id: id,
+        deleted: false
+      });
+
+      return category;
     }
   },
 
@@ -61,6 +81,46 @@ export const resolvers = {
       });
 
       return newArticle;
+    },
+
+    createCategory: async (_, args) => {
+      const { category } = args;
+
+      const record = new Category(category);
+      await record.save();
+
+      return record;
+    },
+
+    deleteCategory: async (_, args) => {
+      const { id } = args;
+
+      await Category.updateOne({
+        _id: id
+      }, {
+        deleted: true
+      });
+
+      return {
+        code: 200,
+        message: "Xóa thành công!"
+      };
+    },
+
+    updateCategory: async (_, args) => {
+      const { id, category } = args;
+
+      await Category.updateOne({
+        _id: id,
+        deleted: false
+      }, category);
+
+      const newCategory = await Category.findOne({
+        _id: id,
+        deleted: false
+      });
+
+      return newCategory;
     }
   }
 };
